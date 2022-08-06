bf369d229656b2ac82eeace57fe0542a5d58e8af
築山博介 <108510020+3181085@users.noreply.github.com>
2022-08-06 15:58:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
+/*******************************************
+ * File:        scriptjs 
+ * Project:     presentaion
+ * Description: the Javascript source code
+ * Mod:         2022.08.06
+ * *****************************************/
 'use strict'
 
 let numOfRow = 0;
-let messageArray = [];
+let pTag = [];
+let arrayOfName = [];
+
+
+let introductionArray = [];
 const defaultInterval = 2000;
 const currentInterval = 1000;
 let id;
-
-// let messageArray = ["Hiroyuki", "Yuusuke", "Syousuke", "Kanta", "Yuna", "Kouhei"];
 let index = 0;
 
-// ウィンドにメッセージを追加する  Interval毎にコールバックされる関数
-let addMessage = () => {
-  let messageBuff;
-  let theMessages = document.getElementById("greetArea");
-  
-  messageBuff = messageArray[index++];
-//  document.write(messageBuff);
-  theMessages.append(messageBuff);
-  console.log("id=", id, " index=", index, "lentght=", messageArray.length);
-  
-  if (index >= messageArray.length) {
-    window.clearInterval(id);
+
+
+/*** onload ***/
+function initFunc() {
+  let option;
+  const listOfCountry = getCountryNames();
+  const select = document.getElementsByName("select");
+
+  // メニューにボタンを追加
+  for (let i=0; i<listOfCountry.length; i++) {
+    option = document.createElement("option");
+    option.innerText = listOfCountry[i];
+    select[0].appendChild(option);
   }
-};
-
-// window.addEventListener("click", () => {
-//    // アロー関数
-//    id = window.setInterval(addMessage, currentInterval);
-// });
+}
 
 
-function initArea(array) {
-  let display;
-  let p;
+/*** submitClick ***/
+function submitClick() {
   let i;
+  const textBox = document.getElementById("guestName");
+  arrayOfName = csvToArr(textBox.value);
 
-  if (numOfRow < array.length) {
-    for (i=numOfRow; i<array.length; i++) {
-      //display = document.getElementById("display");
-      p = document.createElement("p");
-      p.innerText = array[i];
-      document.body.appendChild(p);
-      messageArray[i] = p;
+  if (numOfRow < arrayOfName.length) {
+    for (i=numOfRow; i<arrayOfName.length; i++) {
+      pTag[i] = document.createElement("p");
+      document.body.appendChild(pTag[i]);
     }
     numOfRow = i;
   }
@@ -50,44 +52,82 @@ function initArea(array) {
 
 function clear() {
   for (let i=0; i<numOfRow; i++)
-    messageArray[i].innerText = "";
+    pTag[i].innerText = "";    
 }
 
+
+function show(array) {
+  for (let i=0; i<array.length; i++) 
+    if (array.length > 0)
+      pTag[i].innerText = i+1 + ": " + array[i];
+}
+
+
 function arrayClick() {
-
-  const textBox = document.getElementById("guestName");
-  const array = csvToArr(textBox.value);
-
-  initArea(array); 
   clear();
+  console.log(arrayOfName);
+  show(arrayOfName);
+}
 
-  for (let i=0; i<array.length; i++) {
-    console.log(array[i]);
-    messageArray[i].innerText = array[i];
+
+function sortClick() {
+  clear();
+  show(sort(arrayOfName));
+}
+
+
+function filterClick() {
+  clear();
+  show(arrayOfName.filter(element=>element.match(/[A-Z]/)||element.match(/[a-z]/)));
+}
+
+
+function mapClick() {
+  const select = document.getElementsByName("select");
+  const index = select[0].selectedIndex;
+  if (0 <= index) {
+    const name = select[0].options[index].value
+    clear();
+    show(arrayOfName.map(element=>getPrefixByCountryName(name) + 
+                                " " + element + " " +
+                                getSuffixByCountryName(name)));
   }
 }
 
 
-function greetClick () {
-  console.log("greet")
+
+// セットされたインターバルでコールバックされる
+let showIntroduction = () => {
+  
+  pTag[index].innerText= introductionArray[index];
+  index++;
+
+  // Stop this callback();
+  if (index >= introductionArray.length) 
+    window.clearInterval(id);
+};
+
+
+
+function introductionClick() {
+
+  introductionArray = getIntroductions(arrayOfName);
+  clear();
   index = 0;
-  let element = document.getElementById("geustName");
-  let array = csvToArr(element.value);
-  //let array = sort(csvToArr(element.value));
-  messageArray = getIntroductions(array);
-  id = window.setInterval(addMessage, currentInterval);
+  
+  // インターバルを持たせ、関数を実行させる
+  id = window.setInterval(showIntroduction, currentInterval);
+  //show(newArray);
 }
 
 
-function sortClick () {
-  console.log("sort")
-  index = 0;
-  let element = document.getElementById("geustName");
-  let array = csvToArr(element.value);
-  messageArray = sort(csvToArr(element.value));
-  console.log(messageArray);
-  id = window.setInterval(addMessage, currentInterval);
+function test() {
+  console.log("test");
+  const textBox = document.getElementById("guestName");
+  textBox.value = "鈴木, Sam, Jeams, 田中, David, 三上, 佐藤, 中野, 井上";
+
 }
+
 
 
 /* EOF */
